cc822bd7d2f18b140c5294730d8333f7a4070259
Dmitry Mylnev <Dmitry.Mylnev@Markets.com>
2018-03-30 23:23:52 +0300

    added shell sort

--- src/main/java/com/dvsdimas/app/sort/ShellSort.java
+++ src/main/java/com/dvsdimas/app/sort/ShellSort.java
@@ -8,5 +8,19 @@ public class ShellSort implements ISort {
 
     public void sort(int[] array) {
 
+        for(int gap = array.length / 2; gap > 0; gap /= 2) {
+
+            for (int i = gap; i < array.length; i++) {
+
+                final int element = array[i];
+                int j = i;
+
+                for(; (j >= gap) && (array[j - gap] > element); j -= gap) {
+                    array[j] = array[j - gap];
+                }
+
+                array[j] = element;
+            }
+        }
     }
 }
