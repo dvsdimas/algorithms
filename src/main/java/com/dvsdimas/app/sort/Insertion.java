36a98db14507f22caeace7e201c602aa7738036a
Dmitry Mylnev <Dmitry.Mylnev@Markets.com>
2018-03-30 20:56:16 +0300

    added insertion sort

---
+++ src/main/java/com/dvsdimas/app/sort/Insertion.java
@@ -0,0 +1,26 @@
+package com.dvsdimas.app.sort;
+
+/**
+ * Created by dmylnev on 30.03.18.
+ */
+
+public class Insertion implements ISort {
+
+    public void sort(int[] array) {
+
+        for(int unsorted = 1; unsorted < array.length; unsorted++) {
+
+            int forInsert = array[unsorted];
+
+            int i;
+
+            for(i = unsorted; (i > 0) && (array[i - 1] > forInsert) ;i--) {
+                array[i] = array[i - 1];
+            }
+
+            array[i] = forInsert;
+        }
+
+    }
+
+}
